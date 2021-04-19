ec776dfeb9a46badc1f9170bd74546ccc9a4dc96
Bitpatroon <github@bitpatroon.nl>
2021-04-19 10:24:52 +0200

    Added extension dependency typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,18 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title'                         => 'Login Services',
-    'description'                   => 'Allows technical logging in',
-    'category'                      => 'fe',
-    'author'                        => 'Sjoerd Zonneveld',
-    'author_email'                  => 'typo3@bitpatroon.nl',
-    'state'                         => 'stable',
-    'author_company'                => 'Bitpatroon',
-    'version'                       => '10.3',
+    'title'          => 'Login Services',
+    'description'    => 'Allows technical logging in',
+    'category'       => 'fe',
+    'author'         => 'Sjoerd Zonneveld',
+    'author_email'   => 'typo3@bitpatroon.nl',
+    'state'          => 'stable',
+    'author_company' => 'Bitpatroon',
+    'version'        => '10.4',
+    'constraints'    => [
+        'depends' => [
+            'typo3' => '10.4.0-10.9.9999',
+            'typo3_hooks' => '1.0.0-',
+        ],
+    ],
 ];
